2c0b3f03f915c1f0a3463f64420c41f0d98b7abd
AdithyanA <deepaadithyan56@gmail.com>
2020-08-11 23:59:01 +0530

    sreen display

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,52 @@
 from tkinter import *
 
+# Capturing click value and providing conditions
+
+def click(event):
+    global st_value
+    text = event.widget.cget("text")
+    print(text)
+
+    if text=="=":
+        pass
+    elif text == "C":
+        pass
+
+    else:
+        st_value.set(st_value.get() + text)
+        screen.update()
+
+# Create window and adding title and icon
+
 window = Tk()
-window.geometry("400x500")
-window.title("Calculator")
-window.configure(background="#3b403e")
-
-def hello():
-    print("Button Clicked")
-
-button7=Button(window,text="7", bd=0, command=hello,width=7,height=1)
-button4=Button(window,text="4", bd=0, command=hello,width=7,height=1)
-button1=Button(window,text="1", bd=0, command=hello,width=7,height=1)
-button0=Button(window,text="0", bd=0, command=hello,width=7,height=1)
-button7.grid(row=-0,column=7)
-button4.grid(row=1,column=7)
-button1.grid(row=2,column=7)
-button0.grid(row=3,column=7)
-
-button8=Button(window,text="8",command=hello,width=7,height=1)
-button8.grid(row=0,column=18)
+window.geometry("322x450")
+window.title("ProditorNitro")
+# window.wm_iconbitmap("")
 
 
+# Creating a String to store the value and creating the display screen
 
+st_value = StringVar()
+st_value.set("")
+screen = Entry(window, textvar=st_value, font="lucida 40 bold")
+screen.pack(fill=X, ipadx=8, pady=10, padx=10)
+
+# Adding a frame creating three butttons
+frame1 = Frame(window, background="#3b403e")
+
+button = Button(frame1, text="9", padx=14, pady=11, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=6)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="8", padx=14, pady=11, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=6)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="7", padx=14, pady=11, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=6)
+button.bind("<Button-1>", click)
+
+frame1.pack()
 
 window.mainloop()
 print("hai")
