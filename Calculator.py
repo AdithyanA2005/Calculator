6e84779e1f1a1fdd074619c5e6178c33b914157e
AdithyanA <deepaadithyan56@gmail.com>
2020-08-12 06:31:32 +0530

    all buttons added

--- Calculator.py
+++ Calculator.py
@@ -8,10 +8,18 @@ def click(event):
     print(text)
 
     if text=="=":
-        pass
-    elif text == "C":
-        pass
+        if  st_value.get().isdigit():
+            value = int(st_value.get())
+        else:
+            value = eval(screen.get())
 
+        st_value.set(value)
+        screen.update()
+
+
+    elif text=="C":
+         st_value.set("")
+         screen.update()
     else:
         st_value.set(st_value.get() + text)
         screen.update()
@@ -21,6 +29,7 @@ def click(event):
 window = Tk()
 window.geometry("322x450")
 window.title("ProditorNitro")
+window.background="#000"
 # window.wm_iconbitmap("")
 
 
@@ -34,17 +43,108 @@ screen.pack(fill=X, ipadx=8, pady=10, padx=10)
 # Adding a frame creating three butttons
 frame1 = Frame(window, background="#3b403e")
 
-button = Button(frame1, text="9", padx=14, pady=11, font="lucida 35 bold")
-button.pack(side=LEFT, padx=9, pady=6)
+button = Button(frame1, text="9", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
 button.bind("<Button-1>", click)
 
-button = Button(frame1, text="8", padx=14, pady=11, font="lucida 35 bold")
-button.pack(side=LEFT, padx=9, pady=6)
+button = Button(frame1, text="8", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
 button.bind("<Button-1>", click)
 
-button = Button(frame1, text="7", padx=14, pady=11, font="lucida 35 bold")
-button.pack(side=LEFT, padx=9, pady=6)
+button = Button(frame1, text="7", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
 button.bind("<Button-1>", click)
+
+button = Button(frame1, text="C", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+
+frame1.pack()
+frame1 = Frame(window, background="#3b403e")
+
+
+button = Button(frame1, text="6", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="5", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3 )
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="4", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="=", padx=14, pady=5, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=2)
+button.bind("<Button-1>", click)
+
+
+frame1.pack()
+frame1 = Frame(window, background="#3b403e")
+
+
+button = Button(frame1, text="3", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="2", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="1", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="*", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+
+frame1.pack()
+frame1 = Frame(window, background="#3b403e")
+
+button = Button(frame1, text=".", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="0", padx=18, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="+", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+
+
+
+frame1.pack()
+frame1 = Frame(window, background="#3b403e")
+
+button = Button(frame1, text="/", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+button = Button(frame1, text="%", padx=14, pady=9, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+
+
+button = Button(frame1, text="-", padx=14, pady=11, font="lucida 35 bold")
+button.pack(side=LEFT, padx=9, pady=3)
+button.bind("<Button-1>", click)
+
+frame1.pack()
+frame1 = Frame(window, background="#3b403e")
+
+
+
+
+
+
 
 frame1.pack()
 
